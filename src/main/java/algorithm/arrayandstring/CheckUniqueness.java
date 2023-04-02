c9116a5790daab244095ee9b1dad87f6cde4b459
Zohaldon <47535654+Zohaldon@users.noreply.github.com>
2023-04-02 17:56:20 -0600

    Make variable names more human-readable

--- src/main/java/algorithm/arrayandstring/CheckUniqueness.java
+++ src/main/java/algorithm/arrayandstring/CheckUniqueness.java
@@ -5,27 +5,27 @@ import java.util.ArrayList;
 // An algorithm that determines if a string has all unique characters.
 public class CheckUniqueness {
     // O(N) Time complexity, O(N) Space complexity
-    public boolean isUnique(String str) {
+    public boolean isUnique(String word) {
         // if the string is empty it is unique as it has no duplication.
-        if (str.length() == 0)
+        if (word.length() == 0)
             return true;
 
         // By default, we would treat given string to be unique
-        boolean result = true;
+        boolean isUnique = true;
         ArrayList<Character> ledger = new ArrayList<>();
 
-        for (int i = 0; i < str.length(); i++) {
+        for (int i = 0; i < word.length(); i++) {
             // the comparison is case-insensitive
-            char c = Character.toLowerCase(str.charAt(i));
+            char character = Character.toLowerCase(word.charAt(i));
 
-            if(ledger.contains(c)) {
-                result = false;
+            if(ledger.contains(character)) {
+                isUnique = false;
                 break;
             }
 
-            ledger.add(c);
+            ledger.add(character);
         }
 
-        return result;
+        return isUnique;
     }
 }
