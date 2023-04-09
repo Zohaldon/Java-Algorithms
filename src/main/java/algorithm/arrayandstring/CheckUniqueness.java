b39817177fc8941e78411c6c9f747b86c40bfec6
Zohaldon <47535654+Zohaldon@users.noreply.github.com>
2023-04-09 16:32:59 -0600

    use 2 space for indentation

--- src/main/java/algorithm/arrayandstring/CheckUniqueness.java
+++ src/main/java/algorithm/arrayandstring/CheckUniqueness.java
@@ -4,28 +4,28 @@ import java.util.ArrayList;
 
 // An algorithm that determines if a string has all unique characters.
 public class CheckUniqueness {
-    // O(N) Time complexity, O(N) Space complexity
-    public boolean isUnique(String word) {
-        // if the string is empty it is unique as it has no duplication.
-        if (word.length() == 0)
-            return true;
+  // O(N) Time complexity, O(N) Space complexity
+  public boolean isUnique(String word) {
+    // if the string is empty it is unique as it has no duplication.
+    if (word.length() == 0)
+      return true;
 
-        // By default, we would treat given string to be unique
-        boolean isUnique = true;
-        ArrayList<Character> ledger = new ArrayList<>();
+    // By default, we would treat given string to be unique
+    boolean isUnique = true;
+    ArrayList<Character> ledger = new ArrayList<>();
 
-        for (int i = 0; i < word.length(); i++) {
-            // the comparison is case-insensitive
-            char character = Character.toLowerCase(word.charAt(i));
+    for (int i = 0; i < word.length(); i++) {
+      // the comparison is case-insensitive
+      char character = Character.toLowerCase(word.charAt(i));
 
-            if(ledger.contains(character)) {
-                isUnique = false;
-                break;
-            }
+      if(ledger.contains(character)) {
+        isUnique = false;
+        break;
+      }
 
-            ledger.add(character);
-        }
-
-        return isUnique;
+      ledger.add(character);
     }
+
+    return isUnique;
+  }
 }
