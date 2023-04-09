fb346d5a2ea99d300a14bf3b6b6668420d372db5
Zohaldon <47535654+Zohaldon@users.noreply.github.com>
2023-04-09 16:32:00 -0600

    Add solution for check permutation

---
+++ src/main/java/algorithm/arrayandstring/CheckPermutation.java
@@ -0,0 +1,75 @@
+package algorithm.arrayandstring;
+
+import java.util.Hashtable;
+import java.util.Arrays;
+// Given two strings, the method returns true if one string is a permutation of another
+public class CheckPermutation {
+  // Run time O(N)
+  public boolean isPermutation(String comparand, String permutationCheck){
+    // Handle empty values
+    if (comparand.length() == 0 & permutationCheck.length() == 0)
+      return true;
+    if (comparand.length() == 0 || permutationCheck.length() == 0)
+      return false;
+    // Handle insufficient data
+    if (comparand.length() != permutationCheck.length())
+      return false;
+
+    // If a given string is a permutation of another string
+    // then it should contain all the characters of the other string
+    Hashtable<Character, Integer> checkHash = new Hashtable<Character, Integer>();
+
+    // Fill HashTable using comparand's character. Map each character to number of time it has appeared
+    for(char c: comparand.toCharArray()){
+      if(checkHash.containsKey(c)){
+        int newCharacterCount = checkHash.get(c) + 1;
+        checkHash.replace(c, newCharacterCount);
+      } else {
+        checkHash.put(c, 1);
+      }
+    }
+
+    // Reduce character from hashTable based on how many times it has appeared in comparand
+    for(char c: permutationCheck.toCharArray()){
+      // return false if any unique character is found in permutationCheck string that is not in comparand
+      if(!checkHash.containsKey(c))
+        return false;
+
+      int characterOccurrenceCount = checkHash.get(c);
+      int decreasedCount = characterOccurrenceCount - 1;
+      if(decreasedCount == 0){
+        checkHash.remove(c);
+      }
+      else {
+        checkHash.replace(c, decreasedCount);
+      }
+    }
+
+    // if all characters are found in other string then the resulting hash will be empty
+    return checkHash.isEmpty();
+  }
+
+  // Run time O(N LogN)
+  public boolean isPermutationUsingSort(String comparand, String permutationCheck){
+    // Handle empty values
+    if (comparand.length() == 0 & permutationCheck.length() == 0)
+      return true;
+    if (comparand.length() == 0 || permutationCheck.length() == 0)
+      return false;
+    // Handle insufficient data
+    if (comparand.length() != permutationCheck.length())
+      return false;
+
+    // If a given string is a permutation of another string
+    // then the sorted string should match
+    char[] comparandArray = comparand.toCharArray();
+    char[] permutationCheckArray = permutationCheck.toCharArray();
+    Arrays.sort(comparandArray);
+    Arrays.sort(permutationCheckArray);
+
+    String tempComparand = new String(comparandArray);
+    String tempPermutationCheck = new String(permutationCheckArray);
+
+    return tempComparand.equals(tempPermutationCheck);
+  }
+}
--- src/main/java/algorithm/arrayandstring/Main.java
+++ src/main/java/algorithm/arrayandstring/Main.java
@@ -2,12 +2,28 @@ package algorithm.arrayandstring;
 
 public class Main {
     public static void main(String[] args) {
-        testUniqueness();
+        // testUniqueness();
+        testPermutationUsingONMethod();
     }
 
     public static void testUniqueness() {
         String test_word = "tesT";
         boolean test_result = new CheckUniqueness().isUnique(test_word);
         System.out.println("String: " + test_word + " | isUnique: " + test_result);
+    }
+
+    public static void testPermutationUsingONMethod(){
+        // A: tester | B: ttrees    (true)
+        // A: abcdef | B: dcefba    (true)
+        // A: tester | B: tt        (false)
+        // A: abcdef | B: dcefbavol (false)
+        // A: aab    | B: aaa       (false)
+        String[] comparands =    { "tester", "abcdef", "tester",  "abcdef",    "aab"};
+        String[] permutataions = { "ttrees", "dcefba", "tt",      "dcefbavol", "aaa"};
+
+        for(int i = 0; i < comparands.length; i++) {
+            boolean test_result = new CheckPermutation().isPermutation(comparands[i], permutataions[i]);
+            System.out.println("Comparand: " + comparands[i] + " ; Permutation: " + permutataions[i] + "\t\t\t| isPermutation: " + test_result);
+        }
     }
 }
