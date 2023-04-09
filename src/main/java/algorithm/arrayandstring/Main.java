114abd9c0d49aed26d5276afbc866316ced77ed7
Zohaldon <47535654+Zohaldon@users.noreply.github.com>
2023-04-09 17:36:12 -0600

    Add solution for URLify

--- src/main/java/algorithm/arrayandstring/Main.java
+++ src/main/java/algorithm/arrayandstring/Main.java
@@ -3,7 +3,8 @@ package algorithm.arrayandstring;
 public class Main {
   public static void main(String[] args) {
     // testUniqueness();
-    testPermutation();
+    // testPermutation();
+    testURLified();;
   }
 
   public static void testUniqueness() {
@@ -25,6 +26,23 @@ public class Main {
       // boolean test_result = new CheckPermutation().isPermutation(comparands[i], permutataions[i]);
       boolean test_result = new CheckPermutation().isPermutationUsingSort(comparands[i], permutataions[i]);
       System.out.println("Comparand: " + comparands[i] + " ; Permutation: " + permutataions[i] + "\t\t\t| isPermutation: " + test_result);
+    }
+  }
+
+  public static void testURLified(){
+    // Sample input:   "  Mr John Smith  ", 13
+    // Sample input:   "Mr John Smith    ", 13
+    // Sample output": "Mr%20John%20Smith"
+    String[] inputs = {
+      "  Mr John Smith  ",
+      "Mr John Smith    ",
+      "                 ",
+      "3a2s1d8q5we34&*@3"
+    };
+
+    for(String testInput: inputs){
+      String testOutput = new UrLify().URLifyWhitespace(testInput, 13);
+      System.out.println("Input: " + testInput + " | Output: " +  testOutput);
     }
   }
 }
---
+++ src/main/java/algorithm/arrayandstring/UrLify.java
@@ -0,0 +1,42 @@
+// Replaces all spaces in a given string with '%20'.
+// Where input string would already contain enough space to accommodate the '%20' and the character count is provided
+package algorithm.arrayandstring;
+
+import javax.swing.plaf.synth.SynthTextAreaUI;
+
+public class UrLify {
+  public String URLifyWhitespace(String input, Integer totalExpectedCharacterCount) {
+    if(input.length() == 0)
+      return "";
+
+    StringBuilder urlifiedString = new StringBuilder();
+    boolean replacer = false;
+    int exitCounter = 0;
+
+    for(char c: input.toCharArray()){
+      // If we have filled the stringBuilder then all eligible whitespaces are replaced. Whatever left in the input
+      // string are all whitespace characters at the end.
+     if(exitCounter == totalExpectedCharacterCount)
+        break;
+
+      // As soon as a non whitespace character is found we would want to replace whitespace following it.
+      // By adding replacer flag we can deal with whitespace that are found at the starting of a given string.
+      if(c != ' ') {
+        replacer = true;
+        urlifiedString.append(c);
+        exitCounter++;
+      }
+
+      // If conditions are met then replace the whitespace and turn the replacer flag off.
+      // By turning off the replacer flag the method can avoid replacing consecutive whitespaces.
+      if(c == ' ' && replacer)
+      {
+        replacer = false;
+        urlifiedString.append("%20");
+        exitCounter++;
+      }
+    }
+
+    return urlifiedString.toString();
+  }
+}
