c6c4ecf86e75f673c9c35442472ddd658ad2926b
Zohaldon <parth.pacs@gmail.com>
2023-04-18 22:35:16 -0400

    Add more details to class comment in StringRotation.java

--- src/main/java/algorithm/arrayandstring/StringRotation.java
+++ src/main/java/algorithm/arrayandstring/StringRotation.java
@@ -1,7 +1,8 @@
 // An algorithm that given 2 strings s1 and s2, checks if s2 string is rotation of s1 string by making only 1 call to
-// ~ isSubstring method
+// ~ isSubstring method (contain method for java)
 package algorithm.arrayandstring;
 
+// Runtime: O(N)
 public class StringRotation {
   public static boolean isRotation(String s1, String s2){
     if(s1.length() != s2.length())
